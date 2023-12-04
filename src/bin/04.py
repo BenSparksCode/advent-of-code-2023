9dda440d530ef331c1c9ae7856d394d9e85b55a6
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2023-12-04 09:43:12 +0200

    Solve Day 4 Part 2 in Python

--- src/bin/04.py
+++ src/bin/04.py
@@ -17,7 +17,27 @@ def part_one(input):
     return total
 
 def part_two(input):
-    return None
+    lines = input.split("\n")
+    cardCounts = {} # stores counts as cardNum : cardCount
+
+    for i, line in enumerate(lines, start=1):
+        # Add 1 per card for original card
+        cardCounts[i] = 1 if i not in cardCounts else cardCounts[i] + 1
+
+        # Find number of matches within a card
+        [winNumbers, guesses] = line.split(":")[1].split("|")
+        winNumbers = [int(i.strip()) for i in winNumbers.split(" ") if len(i) > 0]
+        guesses = [int(i.strip()) for i in guesses.split(" ") if len(i) > 0]
+        correctGuesses = 0
+        for guess in guesses:
+            if guess in winNumbers: correctGuesses += 1
+
+        # Adding new cards per existing card in the dictionary
+        for guess_i in range(i + 1, i + 1 + correctGuesses):
+            if guess_i > len(lines): break
+            cardCounts[guess_i] = cardCounts[i] if guess_i not in cardCounts else cardCounts[guess_i] + cardCounts[i]
+
+    return sum(cardCounts.values())
 
 if __name__ == "__main__":
     with open(f"data/inputs/04.txt", "r") as file:
